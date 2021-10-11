8b995e8c297e2721b03ff98e15fd3723a7681b66
pablowaller <juanpablowaller76@gmal.com>
2021-10-11 13:36:35 -0300

    Commit

---
+++ db/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { Provider } from 'react-redux'
+import MainNavigator from './navigation'
+import store from './store'
+
+import { init } from './db'
+
+init()
+.then(() => console.log('Database initialized'))
+.catch(err => {
+    console.log('Database failed to connect');
+    console.log(err.message)
+})
+
+export default function App() {
+    return(<Provider store={store}><MainNavigator/></Provider>)
+}
---
+++ db/index.js
@@ -0,0 +1,44 @@
+import * as SQLite from 'expo-sqlite'
+
+const db = SQLite.openDatabase('address-db');
+
+export const init = () => {
+    const promise = new Promise((resolve, reject) => {
+        db.transaction(
+            (tx) => {
+                tx.executeSql(`CREATE TABLE IF NOT EXISTS address (
+                    id INTEGER PRIMARY KEY NOT NULL,
+                    title TEXT NOT NULL,
+                    image TEXT NOT NULL,
+                    address TEXT NOT NULL, 
+                    lat REAL NOT NULL,
+                    lng REAL NOT NULL
+                )`,
+            [],
+            () => resolve(),
+            (_, err) => reject(err),
+                )},
+        );
+    });
+
+    return promise 
+}
+
+export const insertAddress = (
+    title,
+    image,
+    address,
+    lat,
+    lng,
+    ) => {
+        return new Promise((resolve, reject) => {
+            db.transaction((tx) => {
+                tx.executeSql(
+                    `INSERT INTO address (title, image, address, lat, lng)`,
+                    [],
+                    (_, result) => resolve(result),
+                    (_, error) => reject(error),
+                )
+            })
+        })
+    }
