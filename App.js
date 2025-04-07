41e261484835b39b29822113ce1741148e9c4e2f
pablowaller <76492840+pablowaller@users.noreply.github.com>
2025-04-07 19:46:26 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,19 +1,43 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import { View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
 import { useFonts } from 'expo-font';
-import AppLoading from 'expo-app-loading';
-import MainNavigator from './navigations'
+import MainNavigator from './navigations';
 
 export default function App() {
-  const [loaded] = useFonts({
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  const [fontsLoaded] = useFonts({
     'Panton-LightCaps': require('./assets/fonts/Panton-LightCaps.otf'),
     'OpenSans-Bold': require('./assets/fonts/OpenSans-Bold.ttf')
   });
 
-  if (!loaded) {
-    return <AppLoading />;
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady && fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady, fontsLoaded]);
+
+  if (!appIsReady || !fontsLoaded) {
+    return null;
   }
 
   return (
+    <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
       <MainNavigator />
+    </View>
   );
 }
