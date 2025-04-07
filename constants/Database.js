4258c3d011c0b4f42c17fa0f82a2be44f2faedd1
pablowaller <76492840+pablowaller@users.noreply.github.com>
2025-04-07 19:46:48 -0300

    Add files via upload

--- constants/Database.js
+++ constants/Database.js
@@ -1,25 +1,21 @@
-// Importar los módulos necesarios de Firebase de la nueva versión modular
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getAnalytics } from 'firebase/analytics';
 
-// Configuración de Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyDQJ-amic1aPwLp1B-XyctBgcMRd6ogYwM",
-  authDomain: "sense-bell.firebaseapp.com",
-  databaseURL: "https://sense-bell-default-rtdb.firebaseio.com",
-  projectId: "sense-bell",
-  storageBucket: "sense-bell.firebasestorage.app",
-  messagingSenderId: "874279177046",
-  appId: "1:874279177046:web:4e4d73cf115cb8702224f7",
-  measurementId: "G-D0VE0JN5KQ"
+  apiKey: "AIzaSyD33v7nCUTB9_J0UANMHIHUzQFbpNwaojk",
+  authDomain: "minerva-71d8d.firebaseapp.com",
+  databaseURL: "https://minerva-71d8d-default-rtdb.firebaseio.com",
+  projectId: "minerva-71d8d",
+  storageBucket: "minerva-71d8d.firebasestorage.app",
+  messagingSenderId: "566501806925",
+  appId: "1:566501806925:web:f8534a6f6f8afc46271d04",
+  measurementId: "G-DYRH83NYNT"
 };
 
-// Inicializar Firebase
 const app = initializeApp(firebaseConfig);
 
-// Obtener las instancias de Firestore, Auth y Analytics
 const db = getFirestore(app);
 const auth = getAuth(app);
 const analytics = getAnalytics(app);
